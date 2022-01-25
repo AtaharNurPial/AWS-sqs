2edddc490e6f9f07370eb4848b20230019bedbdf
Atahar Nur Pial <atp82232@gmail.com>
2022-01-25 16:27:02 +0600

    final build(for now)

--- src/CreateFn/app.py
+++ src/CreateFn/app.py
@@ -6,9 +6,10 @@ def lambda_handler(event, context):
     
     client = boto3.client('sqs')
     response = client.send_message(
-        QueueUrl = 'https://sqs.us-east-2.amazonaws.com/618758721119/consoleQueue',
+        QueueUrl = 'https://sqs.us-east-2.amazonaws.com/618758721119/sqs-practice-RawQueue-9TQ7R07SNNU6',
         MessageBody = json.dumps(event["body"]))
     return {
         'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
-        'body': json.dumps(response['ResponseMetadata'])
+        'body': json.dumps(response['ResponseMetadata']),
+        'message': 'message sent...'
     }
--- src/TriggerFn/app.py
+++ src/TriggerFn/app.py
@@ -3,4 +3,9 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     print(event)
+    return{
+        'body':json.dumps({
+            'message': 'message received!!!'
+        })
+    }
    
